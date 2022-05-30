f9985a1c1d41fc4b4f114dc6e555f4d29ee478fd
Dylan <Dylan.zhu@zenlayer.com>
2022-05-30 21:24:30 +0800

    fix

---
+++ docs/component---src-pages-404-js-f1cd0d25c2c9c5b47033.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[50],{w2l6:function(t,e,n){"use strict";n.r(e);var i=n("aD51");e.default=function(){return Object(i.c)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-f1cd0d25c2c9c5b47033.js.map
